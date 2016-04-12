9bfd868f4dd08c933781e59387e8faab994c5a5e
matthewjerome429 <yun.mak.13@ucl.ac.uk>
2016-04-12 12:35:09 +0100

    Connected to database

--- Calendar.php
+++ Calendar.php
@@ -1,5 +1,20 @@
 <html>
 
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "Comp205P";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+    
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+?>
     
     <head>
         <style>
